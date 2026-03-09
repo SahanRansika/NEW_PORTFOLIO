b4e16aa5d7c4c1db8c3c758c5f52f19d8e12533c
SahanRansika <sahanransika07@gmail.com>
2026-03-09 10:40:29 +0530

    My New Portfolio is Completed🎉

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,9 @@
 import { ArrowDown, Download } from "lucide-react";
 
-export const HomeSection = () => {
+export const HeroSection = () => {
   return (
     <section
-      id="home"
+      id="hero"
       className="relative min-h-screen flex flex-col items-center justify-center px-4"
     >
       <div className="container max-w-4xl mx-auto text-center z-10">
